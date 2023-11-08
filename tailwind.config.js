c66540cdc02372e8e4b23c5a326969ca435283c8
Oigbochie Blessed <eprince1800@gmail.com>
2023-11-08 18:40:47 +0100

    done navigation bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+const colors = require('tailwindcss/colors')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
@@ -6,7 +7,19 @@ module.exports = {
       'primary' : '#533625',
       'secondary' : '#28C840',
       'litegreen' : '#84FFA5',
-      'border' : '#FBE068'
+      'border' : '#FBE068',
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      stone: colors.warmGray,
+      sky: colors.lightBlue,
+      neutral: colors.trueGray,
+      gray: colors.coolGray,
+      slate: colors.blueGray,
     },
     extend: {},
   },
