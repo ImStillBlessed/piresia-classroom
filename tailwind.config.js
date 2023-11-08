65d907715fff40818e75750c2ed34c3547047bf9
Oigbochie Blessed <eprince1800@gmail.com>
2023-11-08 17:59:17 +0100

    new commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      'primary' : '#533625',
+      'secondary' : '#28C840',
+      'litegreen' : '#84FFA5',
+      'border' : '#FBE068'
+    },
     extend: {},
   },
   plugins: [],
